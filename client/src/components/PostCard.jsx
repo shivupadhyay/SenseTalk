2402ea116e2669a4ff97dce679a5110da3821770
shiv <shivupadhyay255@gmail.com>
2025-11-14 12:45:25 +0530

    Messages UI Done and Connection page UI started.

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BadgeCheck, Heart, MessageCircle, Share2 } from "lucide-react";
 import moment from "moment";
 import { dummyUserData } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const PostCard = ({ post }) => {
   const postWithHashTags = post.content.replace(
@@ -11,10 +12,16 @@ const PostCard = ({ post }) => {
   const [likes, setLikes] = useState(post.likes_count);
   const currentUser = dummyUserData;
   const handleLike = async () => {};
+
+  const navigate = useNavigate();
+
   return (
     <div className="bg-white rounded-xl shadow p-4 space-y-4 w-full max-w-2xl">
       {/* User Info */}
-      <div className="inline-flex items-center gap-3 cursor-pointer">
+      <div
+        onClick={() => navigate("/profile/" + post.user._id)}
+        className="inline-flex items-center gap-3 cursor-pointer"
+      >
         <img
           src={post.user.profile_picture}
           alt=""
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { dummyConnectionsData } from "../assets/assets";
 import { Eye, MessageSquare } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Messages = () => {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen relative bg-slate-50">
       <div className="max-w-6xl mx-auto p-6">
@@ -29,11 +31,17 @@ const Messages = () => {
                 <p className="text-sm text-gray-600">{user.bio}</p>
               </div>
               <div className="flex flex-col gap-2 mt-4">
-                <button className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1">
-                  <MessageSquare className="w-h h-4"/>
+                <button
+                  onClick={() => navigate(`/messages/${user._id}`)}
+                  className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1"
+                >
+                  <MessageSquare className="w-h h-4" />
                 </button>
-                 <button className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1">
-                  <Eye className="w-h h-4"/>
+                <button
+                  onClick={() => navigate(`/profile/${user._id}`)}
+                  className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer"
+                >
+                  <Eye className="w-h h-4" />
                 </button>
               </div>
             </div>
