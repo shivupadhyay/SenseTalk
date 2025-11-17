1199926cdad55017648f528ad0f410fc9cd331e4
shiv <shivupadhyay255@gmail.com>
2025-11-17 14:58:16 +0530

    Mongoose DB connection done

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import connectDB from "./config/db.js";
 
 const app = express();
+await connectDB();
 
 app.use(express.json());
 app.use(cors());
