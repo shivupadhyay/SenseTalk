c49c425657c7fddb2ee9ed6522a7e46f7120f03e
shiv <shivupadhyay255@gmail.com>
2025-11-17 15:38:45 +0530

    Installation of Inngest for webhooks and for further development.

---
+++ server/Inngest/index.js
@@ -0,0 +1,9 @@
+import { Inngest } from "inngest";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "SenseTalk-app" });
+
+
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [];
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
+import { inngest, functions } from "./Inngest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
 await connectDB();
@@ -10,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => res.send("server is running"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const port = process.env.PORT || 4000;
 
