fd5190abe204765a8c3f979e6692ac28a4c558ab
shiv <shivupadhyay255@gmail.com>
2025-11-21 16:35:58 +0530

    Created Redux store and created slice for every features.

---
+++ client/src/features/user/userSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  value: null,
+};
+
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+});
+
+export default userSlice.reducer;
