70f849f0c60c5f44eaded9fc3e65acd052fe7119
shiv <shivupadhyay255@gmail.com>
2025-11-26 16:39:48 +0530

    Get Recent Messages

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -12,6 +12,7 @@ import {
 } from "../controllers/userController.js";
 import { protect } from "../middlewares/auth.js";
 import { upload } from "../config/multer.js";
+import { getUserRecentMessages } from "../controllers/messageController.js";
 
 const userRouter = express.Router();
 
@@ -31,6 +32,7 @@ userRouter.post("/unfollow", protect, UnfollowUser);
 userRouter.post("/connect", protect, sendConnectionRequest);
 userRouter.post("/accept", protect, acceptConnectionRequest);
 userRouter.get("/connections", protect, getUserConnection);
-userRouter.post("/profiles",protect, getUserProfiles);
+userRouter.post("/profiles", protect, getUserProfiles);
+userRouter.get("/recent-messages", protect, getUserRecentMessages);
 
 export default userRouter;
