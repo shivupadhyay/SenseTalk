831c22fdd04172cf80f8a9ef0d887207bf74fe25
shiv <shivupadhyay255@gmail.com>
2025-12-30 11:27:39 +0530

    Implementing Web Vitals report

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { Provider } from "react-redux";
 import { store } from "./app/store.js";
+import { reportWebVitals } from "./reportWebVitals.js";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -21,3 +22,5 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </ClerkProvider>
 );
+
+console.log(reportWebVitals());
